abd7ddcb45305dc6a6f939783d06760de17faf2e
Kasper van den Berg <kasper@kaspervandenberg.net>
2013-12-02 18:51:44 +0100

    Prepare using ASync server.

--- httpServer.cxx
+++ httpServer.cxx
@@ -1,16 +1,19 @@
 #include <boost/network/protocol/http/server.hpp>
+#include <boost/network/utils/thread_pool.hpp>
+
 #include <boost/program_options.hpp>
 
 namespace http = boost::network::http;
 namespace po = boost::program_options;
 
 
-struct HttpHandler;
+struct SyncHttpHandler;
+struct ASyncHttpHandler;
 
-typedef http::server<HttpHandler> SyncHttpServer;
-typedef http::async_server<HttpHandler> ASyncHttpServer;
+typedef http::server<SyncHttpHandler> SyncHttpServer;
+typedef http::async_server<ASyncHttpHandler> ASyncHttpServer;
 
-struct HttpHandler
+struct SyncHttpHandler
 {
 	void operator()	(
 			const SyncHttpServer::request& request,
@@ -20,6 +23,14 @@ struct HttpHandler
 				SyncHttpServer::response::ok, "Hello World (HTTP)\n");
 	}
 	
+	void log(const SyncHttpServer::string_type& info)
+	{
+		std::cerr << "ERROR " << info << '\n';
+	}
+};
+
+struct ASyncHttpHandler
+{
 	void operator()	(
 			const ASyncHttpServer::request& request,
 			ASyncHttpServer::connection_ptr connection)
@@ -29,24 +40,18 @@ struct HttpHandler
 //				ASyncHttpServer::response::ok, "Hello World (ASync-HTTP)\n");
 //			connection->write(response);
 	}
-	
-
-	void log(const SyncHttpServer::string_type& info)
-	{
-		std::cerr << "ERROR " << info << '\n';
-	}
 };
 
 
 int main(int argc, char * argv[])
 {
-	int port;
+	std::string port;
 	bool asyncServer;
 
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help,h", "display this message")
-		("port,p", po::value<int>(&port)->required(), "port to listen on")
+		("port,p", po::value<std::string>(&port)->required(), "port to listen on")
 		("async", po::value<bool>(&asyncServer)->default_value(false), "use asynchronous server")
 	;
 
@@ -70,18 +75,29 @@ int main(int argc, char * argv[])
 		exit(1);
 	}
 
-	HttpHandler handler;
-
-	SyncHttpServer::options options(handler);
-	options.address("0.0.0.0")
-			.port("8080");
-
-	asyncServer = false;
 	if (asyncServer) {
-//		ASyncHttpServer server (options);
-//		server.run();
+		ASyncHttpHandler handler;
+
+		ASyncHttpServer::options options(handler);
+		options.address("0.0.0.0")
+				.port(port);
+		options.thread_pool(boost::make_shared<boost::network::utils::thread_pool>(2));
+		ASyncHttpServer server (options);
+
+		boost::thread t1(boost::bind(&ASyncHttpServer::run, &server));
+		boost::thread t2(boost::bind(&ASyncHttpServer::run, &server));
+		server.run();
+		t1.join();
+		t2.join();
+
 	} else {
+		SyncHttpHandler handler;
+
+		SyncHttpServer::options options(handler);
+		options.address("0.0.0.0")
+				.port(port);
 		SyncHttpServer server (options);
+
 		server.run();
 	}
 }
