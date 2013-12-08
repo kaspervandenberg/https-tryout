5476d58b8399aa952c15a671148abfca3c067586
Kasper van den Berg <kasper@kaspervandenberg.net>
2013-12-08 17:34:23 +0100

    Decode Base64.

--- syncHttp401Handler.cxx
+++ syncHttp401Handler.cxx
@@ -2,6 +2,69 @@
 
 #include "echoRequest.hxx"
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+#include <boost/archive/iterators/istream_iterator.hpp>
+#include <boost/archive/iterators/ostream_iterator.hpp>
+
+namespace {
+	namespace iter = boost::archive::iterators;
+
+	SyncHttp401Handler::SyncHttpServer::request::header_type findHeaderByName (
+			std::string targetName,
+			const SyncHttp401Handler::SyncHttpServer::request& request)
+	{
+		for (auto header: request.headers) {
+			if (header.name == targetName) {
+				return header;
+			}
+		}
+		throw std::range_error ("Header named " + targetName + " not found");
+	}
+
+	void printAuthorizationHeaderParts (
+			const SyncHttp401Handler::SyncHttpServer::request& request)
+	{
+		try
+		{
+			auto header = findHeaderByName ("Authorization", request);
+			std::cout << "Header name: " << header.name << "\n";
+			
+			std::stringstream headerValue {header.value};
+			std::string authorizationType;
+			std::string base64data;
+			headerValue >> authorizationType >> base64data;
+
+			std::cout << "\t AuthorizationType: " << authorizationType << "\n"
+					<< "\t data (base64): " << base64data << "\n";
+
+			std::cout << "base64 length: " << base64data.size() << "\n\n";
+
+			//iter::binary_from_base64<const unsigned char*>
+			typedef iter::transform_width <
+				iter::binary_from_base64 <
+					std::string::iterator>,
+				8, 6> decodingIter;
+
+			auto iDecodedStart = decodingIter { base64data.begin() };
+			auto iDecodedEnd = decodingIter { base64data.end() };
+			std::string decodedData { iDecodedStart, iDecodedEnd };
+
+			std::cout << "Decoded: " << decodedData << "\n";
+		}
+		catch (const std::range_error& ex)
+		{
+			// Ignore,
+			// No Authorization-header found so do not print anything.
+		}
+	}
+
+
+}
+
 
 void SyncHttp401Handler::operator()	(
 		const SyncHttpServer::request& request,
@@ -13,8 +76,8 @@ void SyncHttp401Handler::operator()	(
 	std::cout << echoRequest(Synchronicity::SYNCHRONE, request);
 	std::cout << "\n<\\end request>\n\n";
 	
-
-
+	printAuthorizationHeaderParts(request);
+	
 	response = SyncHttpServer::response::stock_reply (
 			SyncHttpServer::response::unauthorized,
 			"<html><head><title>Demo: unathorized</title></head>\
