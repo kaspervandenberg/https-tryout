6a8f9916a21de818b4b76577244b5de667791a92
Kasper van den Berg <kasper@kaspervandenberg.net>
2013-12-11 12:14:41 +0100

    Use Boost program options.

--- httpClient.cxx
+++ httpClient.cxx
@@ -1,20 +1,47 @@
 #include <boost/network/protocol/http/client.hpp>
+#include <boost/program_options.hpp>
+
+
 #include <string>
 #include <sstream>
 #include <iostream>
 
 namespace http = boost::network::http;
+namespace po = boost::program_options;
 
 int main(int argc, char * argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " address port" << std::endl;
-        return 1;
-    }
+	std::string host;
+	std::string port;
 
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "display this message")
+		("host,s", po::value<std::string>(&host)->default_value("localhost"), "host to connect to.")
+		("port,p", po::value<std::string>(&port)->default_value("80"), "port to listen on")
+	;
+
+	try {
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		if(vm.count("help")) {
+			desc.print(std::cout);
+			exit(0);
+		}
+		po::notify(vm);
+	} catch (std::exception& ex) {
+		std::cerr << "Missing required option: \n";
+		std::cerr << ex.what() << "\n";
+		
+		std::cerr << "Useage:\n";
+		desc.print(std::cerr);
+
+		exit(1);
+	}
+    
     try {
         http::client client;
         std::ostringstream url;
-        url << "http://" << argv[1] << ":" << argv[2] << "/";
+        url << "http://" << host << ":" << port << "/";
         http::client::request request(url.str());
         http::client::response response =
             client.get(request);
